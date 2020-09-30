f183408eabb574100f4fbe3eb44e134d2394bcc4
BrynnWharton <brynnwharton@u.boisestate.edu>
2020-09-29 21:25:29 -0600

    #12, #13, #14

--- where-were-you/src/app/app.module.ts
+++ where-were-you/src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//custom imports
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivityFeedComponent } from './activity-feed/activity-feed.component';
 import { FriendsComponent } from './friends/friends.component';
 import { EnterActivityComponent } from './enter-activity/enter-activity.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ActivityFeedComponent,
     FriendsComponent,
-    EnterActivityComponent
+    EnterActivityComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
