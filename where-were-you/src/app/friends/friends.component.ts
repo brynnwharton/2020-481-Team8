799baa44b972a34df7b10dfed304fdea82a83af6
BrynnWharton <brynnwharton@u.boisestate.edu>
2020-10-11 14:38:45 -0600

    working on filling the layout with meaningful content

--- where-were-you/src/app/friends/friends.component.ts
+++ where-were-you/src/app/friends/friends.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+function displayPerson(name: string) {
+  console.log(name)
+
+  const newDiv = document.createElement("div")
+  newDiv.appendChild(document.createTextNode(name))
+
+  return newDiv
+}
+
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
@@ -10,6 +19,14 @@ export class FriendsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let friendsList = ["sally", "bob"]
+    friendsList.forEach((value)=>{
+      let parentDiv = document.getElementById("wrapper")
+      let beforeDiv = document.getElementById("add")
+      let newDiv = displayPerson(value)
+      parentDiv.insertBefore(newDiv, beforeDiv)
+    })
+
   }
 
 }
